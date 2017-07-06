7a58067e56cc2e3fa3439bca4c1f74958eb5de46
deltager <github@software.unf.dk>
2017-07-06 11:59:56 +0200

    Initial commit

---
+++ app/src/main/java/software/unf/dk/iter/MainActivity.java
@@ -0,0 +1,13 @@
+package software.unf.dk.iter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
